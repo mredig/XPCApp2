3fb72274e872553c2820e6c190de72ce5e301aca
Michael Redig <mredig@gmail.com>
2019-07-26 12:31:34 -0500

    Initial Commit

---
+++ XPCApp2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XPCApp2
+//
+//  Created by Michael Redig on 7/26/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ XPCApp2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XPCApp2
+//
+//  Created by Michael Redig on 7/26/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
